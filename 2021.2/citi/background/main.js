fff88b20a60b6cc453ef2eb8eb09f075b5bb6291
titi_lima <titisau@gmail.com>
2022-05-18 01:16:46 -0300

    trocador de cores feito :D

---
+++ 2021.2/citi/background/main.js
@@ -0,0 +1,28 @@
+let color = document.getElementById("color");
+let body = document.querySelector("body");
+let rectangle = document.getElementById("rectangle");
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", myFunction);
+
+function myFunction() {
+  switch (color.innerText) {
+    case "#292929":
+      color.innerText = "#F2F2F2";
+      body.style.backgroundColor = "#F2F2F2";
+      rectangle.style.backgroundColor = "#C7C7C7";
+      break;
+    case "#F2F2F2":
+      color.innerText = "#C7C7C7";
+      body.style.backgroundColor = "#C7C7C7";
+      rectangle.style.backgroundColor = "#F2F2F2";
+      break;
+    case "#C7C7C7":
+      color.innerText = "#292929";
+      body.style.backgroundColor = "#292929";
+      rectangle.style.backgroundColor = "#F2F2F2";
+      break;
+  }
+  console.log("sexo");
+}
