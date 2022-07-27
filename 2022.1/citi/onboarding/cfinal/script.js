6e2e819a1f6ab61ab4fc039b888a6b36541a7fe6
titi-lima <titisau@gmail.com>
2022-07-27 00:27:28 -0300

    onboarding3

---
+++ 2022.1/citi/onboarding/cfinal/script.js
@@ -0,0 +1,36 @@
+const bg = document.getElementById("bg");
+const b1 = document.getElementById("b1");
+const body = document.querySelector("body");
+const h1 = document.querySelector("h1");
+const nicetry = document.getElementById("nicetry");
+const t = document.getElementById("t");
+const y = document.getElementById("y");
+const hide = document.getElementsByClassName("hide");
+const mark = document.getElementById("mark");
+const tiago = document.getElementById("first");
+const matheus = document.getElementById("second");
+
+b2 = document.getElementById("b2");
+
+b1.addEventListener("click", () => {
+  if (bg.value === "1a1a1a") {
+    body.style.backgroundColor = "#1a1a1a";
+    nicetry.style.visibility = "visible";
+    bg.parentNode.removeChild(bg);
+    body.removeChild(b1);
+  }
+});
+
+b2.addEventListener("click", () => {
+  if (tiago.value === "tiago" && matheus.value === "matheus") {
+    mark.style.left = "-2vw";
+    t.innerText = "e";
+    t.classList.remove("hide");
+    y.innerText = "agradecem a sua presença!";
+    y.classList.remove("hide");
+    b2.classList.add("hide");
+    setTimeout(() => {
+      window.location.href = "https://youtu.be/NJIzaL3yMrQ?t=100";
+    }, 5000);
+  }
+});
