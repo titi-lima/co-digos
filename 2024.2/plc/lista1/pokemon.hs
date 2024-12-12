a727f416fc71f112770bd6c0f59337116024aea7
titi-lima <titisau@gmail.com>
2024-12-12 17:57:36 -0300

    pokemon update

--- 2024.2/plc/lista1/pokemon.hs
+++ 2024.2/plc/lista1/pokemon.hs
@@ -1,13 +1,13 @@
 pokemon :: String -> String
 pokemon (a:b:c:d:_) = do
-    let ab = read [a, b] :: Int
-    let cd = read [c, d] :: Int
-    let sum = ab + cd
-    let squared = sum ^ 2
-    if squared == (read [a, b, c, d] :: Int)
+    let ab = (read [a] :: Int) * (read [b] :: Int)
+    let cd = (read [c] :: Int) * (read [d] :: Int)
+    let diff = ab - cd
+    let squared = diff ^ 2
+    if squared < (read [a] :: Int) * (read [b] :: Int) * (read [c] :: Int) * (read [d] :: Int)
         then "Charmander vitorioso"
     else "Charmander derrotado"
 
 main = do
     cp <- getLine
-    putStrLn $ pokemon cp
+    print $ pokemon cp
